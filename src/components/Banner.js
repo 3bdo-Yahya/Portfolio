b068b3fd32d1ef11c5a8253ffe29b0707148c611
Mena Ehab <commanderyt123@gmail.com>
2024-11-26 19:04:04 +0200

    fix newsletter section

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -59,7 +59,7 @@ export const Banner = () => {
               {({ isVisible }) => (
                 <div
                   className={
-                    isVisible ? "animated__animated animate__fadeIn" : ""
+                    isVisible ? "animate__animated animate__fadeIn" : ""
                   }
                 >
                   <span className="tagline">Welcome to my Portfolio</span>
--- src/components/Newletters.js
+++ src/components/Newletters.js
@@ -1,25 +1,30 @@
-import React from "react";
-import { Col, Row } from "react-bootstrap";
-import { useState } from "react";
+import React, { useState } from "react";
+import { Col, Row, Alert } from "react-bootstrap";
+
 export const Newletters = ({ onValidated, status, message }) => {
   const [email, setEmail] = useState("");
+  const [alertVisible, setAlertVisible] = useState(false);
+
   const handleSubmit = (e) => {
     e.preventDefault();
     if (email) {
       localStorage.setItem("subscriberEmail", email);
+      setAlertVisible(true);
       clearFields();
+      setTimeout(() => setAlertVisible(false), 3000);
     }
   };
 
   const clearFields = () => {
     setEmail("");
   };
+
   return (
     <Col lg={12} className="">
       <div className="newsletter-bx">
         <Row>
           <Col lg={12} xl={5}>
-            <h3>Subscribe to our Newsletter</h3>
+            <h3>Subscribe to our Newsletter & Never miss latest updates</h3>
           </Col>
           <Col md={6} xl={7}>
             <form onSubmit={handleSubmit}>
@@ -35,6 +40,11 @@ export const Newletters = ({ onValidated, status, message }) => {
             </form>
           </Col>
         </Row>
+        {alertVisible && (
+          <Alert variant="success" className="mt-3">
+            Email successfully registered!
+          </Alert>
+        )}
       </div>
     </Col>
   );
